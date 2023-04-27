73134976a24718e039c7757ace9cdbd4d8477910
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-27 13:42:23 +0300

    Added navigation, header and footer properties. Added some basic pages

---
+++ TradeBazar/ViewComponents/NavigationViewComponent.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace TradeBazar.ViewComponents;
+
+public class NavigationViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(IPublishedContent content)
+    {
+        var homePage = content.AncestorOrSelf<HomePage>();
+        
+        var navigation = homePage?.MainNavigation?
+            .Select(x => (NavigationItem)x.Content)?? Enumerable.Empty<NavigationItem>();
+
+        return View(navigation);
+    }
+}
